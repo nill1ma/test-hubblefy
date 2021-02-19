d9b072e9cece810f138a0ba0e2e60b42153957a1
Nil <avanilson.limasantos@gmail.com>
2021-02-19 07:19:35 -0300

    Criação de arquivo de constantes para os endereços de imagens

---
+++ src/components/SlideBanner/img-consts.js
@@ -0,0 +1,5 @@
+export const slideItems = [
+    { description: 'Hub de informações', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMOOzoPKi_-JQbzw1E2usEmnAqalpby5NXQA&usqp=CAU' },
+    { description: 'Informações sempre disponíveis', img: 'https://media-exp1.licdn.com/dms/image/C4D1BAQGr2wjnli4diQ/company-background_10000/0/1575225857515?e=2159024400&v=beta&t=h-_TvgwM5DelIzOCIPy9WfYzTw2sSr28LKyh_1ae5to' },
+    
+]
